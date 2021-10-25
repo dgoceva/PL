fdfda59a11eb5e4be1ffece0594392462a3aeee6
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-25 10:23:05 +0300

    random value added

--- arrayDemo.c
+++ arrayDemo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_LEN 10
 
@@ -8,14 +9,28 @@ void main()
     int data[MAX_LEN];
     int i;
 
+    // for (i = 0; i < MAX_LEN; i++)
+    // {
+    //     printf("A[%d]=", i + 1);
+    //     scanf("%d", &data[i]);
+    // }
+
+    srand((unsigned int)time(NULL));
+
     for (i = 0; i < MAX_LEN; i++)
     {
-        printf("A[%d]=", i + 1);
-        scanf("%d", &data[i]);
+        data[i] = rand() % 100;
     }
 
     for (i = 0; i < MAX_LEN; i++)
     {
-        print("%d\t", data[i]);
+        printf("%d\t", data[i]);
     }
+    printf("\n");
+
+    for (i = MAX_LEN - 1; i >= 0; i--)
+    {
+        printf("%d\t", data[i]);
+    }
+    printf("\n");
 }
