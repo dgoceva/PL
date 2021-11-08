a16c2d41db8d4d23bd89b2f9b31b2a68fda080fb
dgoceva <dgoceva@abv.bg>
2021-11-08 10:12:52 +0200

    power function added

--- arrayDemo.c
+++ arrayDemo.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define MAX_LEN 30
+#define MAX_LEN 10
+
+typedef enum
+{
+    FALSE,
+    TRUE
+} boolean;
 
 void inputArray(int *data);
 void printArray(int *data);
 float averageArray(int *data);
 int minArray(int *data);
 int maxArray(int *data);
+void powerArray(int *in, int *out, int power);
+boolean isEven(int number);
+unsigned int evenArray(int *in, int *out);
 
 void main()
 {
     int data[MAX_LEN];
+    int output[MAX_LEN];
 
     inputArray(data);
     printArray(data);
     printf("Average T is %.2g\n", averageArray(data));
     printf("Min T is %d\tMax T is %d\n", minArray(data), maxArray(data));
+
+    powerArray(data, output, 2);
+    printArray(output);
 }
 
 void inputArray(int *data)
@@ -78,4 +92,13 @@ int maxArray(int *data)
         }
     }
     return max;
+}
+
+void powerArray(int *in, int *out, int power)
+{
+    int i;
+    for (i = 0; i < MAX_LEN; i++)
+    {
+        out[i] = (int)pow(in[i], power);
+    }
 }
